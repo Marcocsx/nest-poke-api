d7c3a445584b7e88c60a42f2d5e452cd64844be4
marcovizcarra <mvizcarrap@tecsup.edu.pe>
2024-10-20 18:39:50 -0500

    first commit

---
+++ src/seed/seed.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@nestjs/common';
+import { HttpService } from '@nestjs/axios';
+import { PokeResponse } from './interfaces/poke-response.interface';
+import { firstValueFrom, lastValueFrom } from 'rxjs';
+
+
+@Injectable()
+export class SeedService {
+
+  constructor(
+    private readonly http: HttpService,
+  ) { }
+
+  async populateDB() {
+
+    const { data } = await firstValueFrom(this.http.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=650'));
+
+    data.results.forEach(({ name, url }) => {
+      const segmentes = url.split('/');
+      const no:number = +segmentes[segmentes.length - 2];
+      console.log({ name, no })
+    });
+
+    return data.results;
+  }
+
+}
