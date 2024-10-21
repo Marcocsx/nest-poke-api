58fc20f7fe0ba83532e8bcddeb7e6ed2f773c0a0
marcovizcarra <mvizcarrap@tecsup.edu.pe>
2024-10-20 21:59:37 -0500

    first commit

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const port = process.env.PORT ?? 3000;
   app.setGlobalPrefix('api/v2')
   app.useGlobalPipes(
     new ValidationPipe({
@@ -15,6 +16,7 @@ async function bootstrap() {
       }
     })
   );
-  await app.listen(3000);
+  await app.listen(port);
+  Logger.log(`App running on port ${port}`)
 }
 bootstrap();
